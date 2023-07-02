2250d7203cfea879f478d586f373b72b8f7efa21
sithembiso <31868918+pro-lover@users.noreply.github.com>
2023-07-03 00:57:31 +0200

    latest c

--- Photoshop/ExportLayerCoordinatesPhotoshop.jsx
+++ Photoshop/ExportLayerCoordinatesPhotoshop.jsx
@@ -38,12 +38,7 @@ for (var i=0; i< artboards.length; i++)
   var fileType;
   var win = new Window("dialog", "CMDBANNER.IO EXPORT OPTIONS", [150, 150, 460, 650]);
    
-  //Changing text
-  var visibleArtboard = docRef.layerSets.getByName(visibleArtboards[0]);
-  var titleGroup = visibleArtboard.layers.getByName("copy");
-
-  var titleLayer = titleGroup.layers[0];
-  var copyContent = titleLayer.textItem.contents;
+  var NoOfCopy = 0;
 
   // Add a panel
 	win.radioPanel = win.add("panel", [25, 105, 285, 230], "Export Options");
@@ -52,11 +47,9 @@ for (var i=0; i< artboards.length; i++)
 
 	// Add checkboxes.contents
 	win.statictextPanel.statictextOne = win.statictextPanel.add("statictext", [10, 15, 300, 35], "Edit copy");
-  win.statictextPanel.edittextOne = win.statictextPanel.add('edittext {properties: {name: "edittextOne"}}', [10, 45, 300, 65] ); 
-  win.statictextPanel.edittextOne.text = copyContent; 
 
-
-	win.statictextPanel.statictextFour = win.statictextPanel.add("statictext", [10, 75, 300, 95], "Rasterize Layers");
+  win.statictextPanel.edittext = win.statictextPanel.add('edittext {properties: {name: "edittext"}}', [10, 45, 200, 65] ); 
+  win.statictextPanel.edittext.text = NoOfCopy; 
 
 
 	// Add radio buttons
@@ -97,6 +90,8 @@ for (var i=0; i< artboards.length; i++)
 		}
 		//win.radioPanel.radTxtOne.text = selected;
 	};
+
+  
   win.folderPanel.btnFolderInput.onClick = function () {
     f = Folder.selectDialog();
     if (f) {
@@ -113,23 +108,116 @@ for (var i=0; i< artboards.length; i++)
   }
 
 	if(win.show() == 1) {
-    Process(docName,visibleArtboards);
+    ProcessNoOfCopy(win.statictextPanel.edittext.text);
   }
 
-  function Process(docName,visibleArtboards) {
-    
-    for(var i=0;i<visibleArtboards.length;i++)
-    {
-      alert(visibleArtboards[i]);
-    }
+  function ProcessNoOfCopy(NoOfCopy) { 
+    var  f;
+    var fileType;
+    for ( var x = 0; x < visibleArtboards.length; x++ ) {
 
-    titleLayer.textItem.contents = win.statictextPanel.edittextOne.text;
+      var win = new Window("dialog", "Edit Copy", [150, 150, 460, 70*NoOfCopy+310]);
+      
+      //Changing text
+      var visibleArtboard = docRef.layerSets.getByName(visibleArtboards[x]);
+      var titleGroup = visibleArtboard.layers.getByName("TXT");
+
+      // Add a panel
+      win.statictextPanel = win.add("panel", [25, 25, 285, 60*(NoOfCopy)+60], "Edit copy");
+
+      var titleLayer = [];
+      var copyContent = [];
+      
+      for(var i=0; i<NoOfCopy;i++) {
+
+        titleLayer[i] = titleGroup.layers[i];
+        copyContent[i] = titleLayer[i].textItem.contents;
+
+
+        if (i == 0) {
+          win.statictextPanel.statictext0 = win.statictextPanel.add("statictext", [10, 15*(i+1), 300, 35], "Edit copy "+(i+1));
+          win.statictextPanel.edittext0 = win.statictextPanel.add('edittext {properties: {name: "edittext0"}}', [10, 45*(1+i), 200, 65] ); 
+          win.statictextPanel.edittext0.text = copyContent[i]; 
+          titleLayer[i].textItem.contents = win.statictextPanel.edittext0.text;
+        }
+        if (i == 1) {
+          win.statictextPanel.statictext1 = win.statictextPanel.add("statictext", [10, 15*(i+1), 300, 35], "Edit copy "+(i+1));
+          win.statictextPanel.edittext1 = win.statictextPanel.add('edittext {properties: {name: "edittext1"}}', [10, 45*(1+i), 200, 65] ); 
+          win.statictextPanel.edittext1.text = copyContent[i]; 
+          titleLayer[i].textItem.contents = win.statictextPanel.edittext1.text;
+        }
+        if (i == 2) {
+          win.statictextPanel.statictext2 = win.statictextPanel.add("statictext", [10, 15*(i+1), 300, 35], "Edit copy "+(i+1));
+          win.statictextPanel.edittext2 = win.statictextPanel.add('edittext {properties: {name: "edittext2"}}', [10, 45*(1+i), 200, 65] ); 
+          win.statictextPanel.edittext2.text = copyContent[i]; 
+          titleLayer[i].textItem.contents = win.statictextPanel.edittext2.text;
+        }
+        if (i == 3) {
+          win.statictextPanel.statictext3 = win.statictextPanel.add("statictext", [10, 15*(i+1), 300, 35], "Edit copy "+(i+1));
+          win.statictextPanel.edittext3 = win.statictextPanel.add('edittext {properties: {name: "edittext3"}}', [10, 45*(1+i), 200, 65] ); 
+          win.statictextPanel.edittext3.text = copyContent[i]; 
+          titleLayer[i].textItem.contents = win.statictextPanel.edittext3.text;
+        }
+        if (i == 4) {
+          win.statictextPanel.statictext4 = win.statictextPanel.add("statictext", [10, 15*(i+1), 300, 35], "Edit copy "+(i+1));
+          win.statictextPanel.edittext4 = win.statictextPanel.add('edittext {properties: {name: "edittext4"}}', [10, 45*(1+i), 200, 65] ); 
+          win.statictextPanel.edittext4.text = copyContent[i]; 
+          titleLayer[i].textItem.contents = win.statictextPanel.edittext4.text;
+        }
+        if (i == 5) {
+          win.statictextPanel.statictext5 = win.statictextPanel.add("statictext", [10, 15*(i+1), 300, 35], "Edit copy "+(i+1));
+          win.statictextPanel.edittext5 = win.statictextPanel.add('edittext {properties: {name: "edittext5"}}', [10, 45*(1+i), 200, 65] ); 
+          win.statictextPanel.edittext5.text = copyContent[i]; 
+          titleLayer[i].textItem.contents = win.statictextPanel.edittext5.text;
+        }
+        if (i == 6) {
+          win.statictextPanel.statictext6 = win.statictextPanel.add("statictext", [10, 15*(i+1), 300, 35], "Edit copy "+(i+1));
+          win.statictextPanel.edittext6 = win.statictextPanel.add('edittext {properties: {name: "edittext6"}}', [10, 45*(1+i), 200, 65] ); 
+          win.statictextPanel.edittext6.text = copyContent[i]; 
+          titleLayer[i].textItem.contents = win.statictextPanel.edittext6.text;
+        }
+      }
+    
+      // bounds = [left, top, right, bottom]
+      win.btnPanel = win.add("panel", [25, 70*NoOfCopy+60, 285, 80*NoOfCopy+100], );
+      win.btnPanel.orientation = "row";
+      // Add the components, two buttons
+      win.btnPanel.okBtn = win.btnPanel.add("button", [10, 15, 100, 35], "CONTINUE");
+      win.btnPanel.cancelBtn = win.btnPanel.add("button", [150, 15, 220, 35], "CANCEL");
+      // Register event listeners that define the button behavior
+        // Event listener for the radio buttons
+    
+      win.btnPanel.okBtn.onClick = function () {
+        win.close(1);
+      }
+    
+      win.btnPanel.cancelBtn.onClick = function () {
+        win.close(0);
+      }
+    
+      if(win.show() == 1) {
+        Process(docName,visibleArtboards[x]);
+      }
+    }
+  }
+
+  function Process(docName,fname) {
+    
+    
+    exportOutput(docName,fname)
+      
   }
 
   // Output
-  function exportOutput(docName) {
+  function exportOutput(docName,fname) {
     if(fileType != undefined){
       if(fileType == ".jpg"){
+      
+         // var docExportOptions = new ExportOptionsSaveForWeb;
+        //  var destFile = new File(f.fullName  + fname + ".jpg");
+         // app.activeDocument.saveAs (destFile, docExportOptions, true, Extension.LOWERCASE);
+        
+
         var file = new File(f.fullName + '/' + docName + fileType);
         var opts = new JPEGSaveOptions();
         opts.quality = 10;
